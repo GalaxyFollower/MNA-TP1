d39b0c0b27a48dab7e31a80d7919d3d48835daf2
Victoria <mvictoria.cavo@gmail.com>
2015-10-08 14:39:55 -0300

    return variable name conflict fixed

--- Brusselator/Common/prb2.m
+++ Brusselator/Common/prb2.m
@@ -5,11 +5,11 @@ function E = eigBrusselatorJ (m,L,delta1,delta2,alpha,beta)
     	for j=1:m,
        		eigofT(j) = -2*(1- cos(pi*j*h) );  % eigenvalues of T
     	end;
-    	for j=1:m,
+    	for j=1:m
       		coeff(1) = 1;
        		coeff(2) = alpha^2 - (beta - 1) - (tau1+tau2)*eigofT(j);
        		coeff(3) = beta*alpha^2 + tau1*tau2*eigofT(j)^2 + tau2*(beta-1)*eigofT(j) - alpha^2*tau1*eigofT(j) - alpha^2*(beta-1);
        		d = roots(coeff);
-       		Aeig(j) = d(1);  Aeig(m+j) = d(2); % eigenvalues of A
+       		E(j) = d(1);  E(m+j) = d(2); % eigenvalues of A
     	end;
 endfunction
