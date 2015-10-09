4424a0dace910fc05ba63f6cac671f7128a34639
Victoria <mvictoria.cavo@gmail.com>
2015-10-09 00:29:23 -0300

    test changed

--- Brusselator/Common/test.m
+++ Brusselator/Common/test.m
@@ -1,16 +1,11 @@
 
-function [E1,E2] = test(m,L,delta1,delta2,alpha,beta)
-	E1 = eigBrusselatorJ(m,L,delta1,delta2,alpha,beta);
-	E2 = eigBrusselatorJ4(m,L,delta1,delta2,alpha,beta);
-endfunction
+function E = eigtest(bool,m,tol);
+	if (bool == 1)
+		E = eigAnalytic(m,0.51302,0.008,0.004,2,5.45);
+	else
+		E = eigNumeric(m,0.51302,0.008,0.004,2,5.45,tol);
+	end		
+end
 
-function [E1,E2]  =  test1 ()
-	[E1,E2] = test(100,0.51302,0.008,0.004,2,5.45);
-endfunction
-
-
-function [E1,E2] = test2 ()
-	[E1,E2] = test(1000,0.51302,0.008,0.004,2,5.45);
-endfunction 
 
 
