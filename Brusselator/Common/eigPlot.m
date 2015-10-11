cad4d7104ed96479edd7bd8f28a65c2c5ea5d6e3
Viki <mvictoria.cavo@gmail.com>
2015-10-10 23:05:50 -0300

    Update eigPlot.m

--- Brusselator/Common/eigPlot.m
+++ Brusselator/Common/eigPlot.m
@@ -1,4 +1,4 @@
-function = eigPlot (en,ea) 
-	plot(real(en),imag(en),'r.',real(ea),imag(ea),'b.')
-	xlabel('Eje Real'); ylabel('Eje Imaginario'); legend('Eigenvalores numéricos','Eigenvalores Analíticos')
+function en1 = eigPlot (en1,en2,en3,en4,ea) 
+	plot(real(en1),imag(en1),'r.',real(en2),imag(en2),'g.',real(en3),imag(en3),'c.',real(en4),imag(en4),'m.',real(ea),imag(ea),'b.')
+	xlabel('Eje Real'); ylabel('Eje Imaginario'); legend('Numericos con tolerancia 1e-4','Numericos con tolerancia 1e-6','Numericos con tolerancia 1e-8','Numericos con tolerancia 1e-10','Analiticos')
 end
