c4dda98419abac7f5f9da5a9c5c287604469fa03
Viki <mvictoria.cavo@gmail.com>
2015-10-12 02:04:51 -0300

    Update brusselatorJMatrixTest.m

--- Brusselator/Common/brusselatorJMatrixTest.m
+++ Brusselator/Common/brusselatorJMatrixTest.m
@@ -6,7 +6,7 @@ function b = brusselatorJMatrixTest()
 	aux = all(A200-B200);
 	b='success';
 	for k=1:length(aux)
-		if aux(k)!=0
+		if (aux(k)!=0)
 			b='failed bwm200.mtx';
 		end
 	end
@@ -16,7 +16,7 @@ function b = brusselatorJMatrixTest()
         	B2000 = brusselatorJMatrix(1000,0.51302,0.008,0.004,2,5.45);
         	aux = all(A2000-B2000);
         	for k=1:length(aux)
-                	if aux(k)!=0
+                	if (aux(k)!=0)
                         	b='failed bwm2000.mtx';
                 	end
         	end
